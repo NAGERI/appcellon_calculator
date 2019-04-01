353bc85e8d30b03f05dd72069c4296f436b93008
NAGERI <onacedricnageri@gmail.com>
2019-04-01 18:57:53 +0300

    completed step A of a basic calculator

---
+++ appcellonCalc.py
@@ -0,0 +1,12 @@
+from math import *
+
+print('Select Operation :')
+print('ADD')
+print('SUBTRACT')
+print('MULTIPLY')
+print('DIVIDE')
+
+# a: int = input('Enter first value : ')
+# b: int = input('Enter next value : ')
+#
+# print(a * b)
